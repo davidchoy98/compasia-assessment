3a8f86f7a3819d37eb1e9ac77c4ebbef5ea185cb
David Choy <38069452+davidchoy98@users.noreply.github.com>
2024-06-24 00:36:30 +0800

    Added Migration Files

--- database/migrations/2024_06_21_163928_create_product_types_table.php
+++ database/migrations/2024_06_21_163928_create_product_types_table.php
@@ -11,15 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::defaultStringLength(191);
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('product_types', function (Blueprint $table) {
             $table->id();
+
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
@@ -28,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('product_types');
     }
 };
--- database/migrations/2024_06_21_164151_create_product_models_table.php
+++ database/migrations/2024_06_21_164151_create_product_models_table.php
@@ -11,15 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::defaultStringLength(191);
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('product_models', function (Blueprint $table) {
             $table->id();
+
+            $table->unsignedBigInteger('capacity_id')->references('id')->on('capacities');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
@@ -28,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('product_models');
     }
 };
